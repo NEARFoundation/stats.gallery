5839f970fe1e20de7b0796766a23b9960325dd92
Jacob <encody@users.noreply.github.com>
2021-06-14 11:03:41 -0500

    extract styles & NetworkSelector component

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,142 +1,202 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
-    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-      <div class="relative flex items-center justify-between h-16">
-        <div
-          class="
-            sm:flex-1
-            flex
-            items-center
-            justify-center
-            sm:items-stretch sm:justify-start
-          "
-        >
-          <!-- Brand icon -->
-          <div class="flex-shrink-0 flex items-center">
-            <img
-              class="block lg:hidden h-8 w-auto"
-              src="../assets/near_icon_wht.svg"
-              alt="Workflow"
-            />
-            <img
-              class="hidden lg:block h-8 w-auto"
-              src="../assets/near_icon_wht.svg"
-              alt="Workflow"
-            />
-          </div>
-
-          <!-- Nav links -->
-          <div class="hidden sm:block sm:ml-6">
-            <div class="flex space-x-4">
-              <a
-                v-for="item in navigation"
-                :key="item.name"
-                :href="item.href"
-                :class="[
-                  item.current
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'px-3 py-2 rounded-md text-sm font-medium',
-                ]"
-                :aria-current="item.current ? 'page' : undefined"
-                >{{ item.name }}</a
-              >
-            </div>
-          </div>
+    <div class="top-bar">
+      <!-- Navigation links -->
+      <div class="nav">
+        <!-- Brand icon -->
+        <div class="nav__brand">
+          <img
+            class="nav__image"
+            src="../assets/near_icon_wht.svg"
+            alt="NEAR Edu"
+          />
         </div>
 
-        <!-- Search section -->
-        <div class="flex-1 flex justify-center sm:justify-end">
-          <div class="w-full px-2 lg:px-6">
-            <label for="search" class="sr-only">Input account name</label>
-            <div class="relative text-indigo-200 focus-within:text-gray-400">
-              <div
-                class="
-                  absolute
-                  inset-y-0
-                  left-0
-                  pl-3
-                  flex
-                  items-center
-                  pointer-events-none
-                "
-              >
-                <SearchIcon class="h-5 w-5" aria-hidden="true" />
-              </div>
-              <input
-                id="search"
-                name="search"
-                class="
-                  block
-                  w-full
-                  pl-10
-                  pr-3
-                  py-2
-                  border border-transparent
-                  rounded-md
-                  leading-5
-                  bg-indigo-400 bg-opacity-25
-                  text-indigo-100
-                  placeholder-indigo-200
-                  focus:outline-none
-                  focus:bg-white
-                  focus:ring-0
-                  focus:placeholder-gray-400
-                  focus:text-gray-900
-                  sm:text-sm
-                "
-                placeholder="account.near&hellip;"
-                type="search"
-              />
-            </div>
-          </div>
-        </div>
-
-        <!-- Mobile menu button-->
-        <div class="flex items-center sm:hidden">
-          <DisclosureButton
-            class="
-              inline-flex
-              items-center
-              justify-center
-              p-2
-              rounded-md
-              text-gray-400
-              hover:text-white hover:bg-gray-700
-              focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white
-            "
+        <!-- Nav links -->
+        <div class="nav__links">
+          <a
+            v-for="item in navigation"
+            :key="item.name"
+            :href="item.href"
+            :class="[
+              item.current ? 'nav__link--active' : 'nav__link--inactive',
+              'nav__link',
+            ]"
+            :aria-current="item.current ? 'page' : undefined"
+            >{{ item.name }}</a
           >
-            <span class="sr-only">Open main menu</span>
-            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
-          </DisclosureButton>
         </div>
       </div>
+
+      <!-- Search section -->
+      <div class="search">
+        <label for="search" class="sr-only">Input account name</label>
+        <!-- Input wrapper -->
+        <div class="search__input-wrapper">
+          <!-- Search icon -->
+          <div class="search__icon">
+            <SearchIcon class="h-5 w-5" aria-hidden="true" />
+          </div>
+
+          <!-- Search field -->
+          <input
+            id="search"
+            name="search"
+            class="search__input"
+            placeholder="account.near&hellip;"
+            type="search"
+          />
+        </div>
+
+        <!-- Network selector -->
+        <NetworkSelector />
+      </div>
+
+      <!-- Mobile menu button-->
+      <DisclosureButton class="mobile-menu-button">
+        <span class="sr-only">Open main menu</span>
+        <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+        <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+      </DisclosureButton>
     </div>
 
-    <DisclosurePanel class="sm:hidden">
-      <div class="px-2 pt-2 pb-3 space-y-1">
-        <router-link
-          v-for="item in navigation"
-          :key="item.name"
-          :href="item.href"
-          :class="[
-            item.current
-              ? 'bg-gray-900 text-white'
-              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-            'block px-3 py-2 rounded-md text-base font-medium',
-          ]"
-          :aria-current="item.current ? 'page' : undefined"
-          >{{ item.name }}</router-link
-        >
-      </div>
+    <!-- Mobile menu -->
+    <DisclosurePanel class="mobile-menu">
+      <a
+        v-for="item in navigation"
+        :key="item.name"
+        :href="item.href"
+        :class="[
+          'mobile-menu__item',
+          item.current
+            ? 'mobile-menu__item--active'
+            : 'mobile-menu__item--inactive',
+        ]"
+        :aria-current="item.current ? 'page' : undefined"
+        >{{ item.name }}</a
+      >
     </DisclosurePanel>
   </Disclosure>
 </template>
 
+<style lang="scss" scoped>
+.top-bar {
+  @apply relative
+    flex
+    items-center
+    justify-between
+    h-16
+    max-w-7xl
+    mx-auto
+    px-2
+    sm:px-6
+    lg:px-8;
+}
+
+.nav {
+  @apply sm:flex-1
+    flex
+    items-center
+    justify-center
+    sm:items-stretch sm:justify-start;
+
+  &__brand {
+    @apply flex-shrink-0 flex items-center;
+  }
+
+  &__image {
+    @apply block h-8 w-auto;
+  }
+
+  &__links {
+    @apply space-x-4 hidden sm:flex sm:ml-6;
+  }
+
+  &__link {
+    @apply px-3 py-2 rounded-md text-sm font-medium;
+
+    &--inactive {
+      @apply text-gray-300 hover:bg-gray-700 hover:text-white;
+    }
+
+    &--active {
+      @apply bg-gray-900 text-white;
+    }
+  }
+}
+
+.search {
+  @apply rounded-md
+    shadow-sm
+    w-full
+    px-2
+    lg:px-6
+    flex-1 flex
+    justify-center
+    sm:justify-end;
+
+  &__input-wrapper {
+    @apply relative
+      flex-grow
+      text-indigo-200
+      focus-within:text-gray-400;
+  }
+
+  &__icon {
+    @apply absolute
+      inset-y-0
+      left-0
+      pl-3
+      flex
+      items-center
+      pointer-events-none;
+  }
+
+  &__input {
+    @apply block
+      w-full
+      pl-10
+      pr-3
+      py-2
+      rounded-l-md
+      border border-transparent
+      leading-5
+      bg-indigo-400 bg-opacity-25
+      text-indigo-100
+      placeholder-indigo-200
+      focus:outline-none
+      focus:bg-white
+      focus:ring-0
+      focus:placeholder-gray-400
+      focus:text-gray-900
+      sm:text-sm;
+  }
+}
+
+.mobile-menu-button {
+  @apply sm:hidden inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white;
+}
+
+.mobile-menu {
+  @apply sm:hidden px-2 pt-2 pb-3 space-y-1;
+
+  &__item {
+    @apply block px-3 py-2 rounded-md text-base font-medium;
+
+    &--inactive {
+      @apply text-gray-300 hover:bg-gray-700 hover:text-white;
+    }
+
+    &--active {
+      @apply bg-gray-900 text-white;
+    }
+  }
+}
+</style>
+
 <script lang="ts">
 import { ref } from 'vue';
+import NetworkSelector from './NetworkSelector.vue';
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
 import { MenuIcon, XIcon, SearchIcon } from 'heroicons-vue3/outline';
 
@@ -148,6 +208,7 @@ const navigation = [
 
 export default {
   components: {
+    NetworkSelector,
     Disclosure,
     DisclosureButton,
     DisclosurePanel,
---
+++ src/components/NetworkSelector.vue
@@ -0,0 +1,108 @@
+<template>
+  <Menu as="div" class="network-selector">
+    <div>
+      <!-- Network selector button -->
+      <MenuButton class="network-selector__button">
+        mainnet
+        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+      </MenuButton>
+    </div>
+
+    <!-- Network selector options -->
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-from-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-from-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+    >
+      <MenuItems class="network-selector__menu">
+        <div class="py-1">
+          <MenuItem v-slot="{ active }">
+            <a
+              href="#"
+              :class="[
+                active
+                  ? 'network-selector__option--active'
+                  : 'network-selector__option--inactive',
+                'network-selector__option',
+              ]"
+              >mainnet</a
+            >
+          </MenuItem>
+          <MenuItem v-slot="{ active }">
+            <a
+              href="#"
+              :class="[
+                active
+                  ? 'network-selector__option--active'
+                  : 'network-selector__option--inactive',
+                'network-selector__option',
+              ]"
+              >testnet</a
+            >
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
+
+<style lang="scss" scoped>
+.network-selector {
+  @apply relative inline-block text-left;
+
+  &__button {
+    @apply inline-flex
+      justify-center
+      w-full
+      rounded-r-md
+      shadow-sm
+      px-4
+      py-2
+      bg-transparent
+      text-sm
+      font-medium
+      border border-transparent
+      leading-5
+      bg-indigo-400 bg-opacity-25
+      text-indigo-100
+      placeholder-indigo-200
+      hover:bg-indigo-500 hover:bg-opacity-25 hover:text-white
+      focus:bg-indigo-500 focus:bg-opacity-25 focus:text-white
+      sm:text-sm;
+  }
+
+  &__menu {
+    @apply origin-top-right z-10 absolute right-0 mt-2 w-28 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none;
+  }
+
+  &__option {
+    @apply block px-4 py-2 text-sm;
+
+    &--inactive {
+      @apply text-gray-700;
+    }
+
+    &--active {
+      @apply bg-gray-100 text-gray-900;
+    }
+  }
+}
+</style>
+
+<script lang="ts">
+import { Menu, MenuItem, MenuItems, MenuButton } from '@headlessui/vue';
+import { ChevronDownIcon } from 'heroicons-vue3/outline';
+
+export default {
+  components: {
+    Menu,
+    MenuItem,
+    MenuItems,
+    MenuButton,
+    ChevronDownIcon,
+  },
+};
+</script>
