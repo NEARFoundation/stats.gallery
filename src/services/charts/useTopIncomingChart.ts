882351d4cb3584add1511108230e8bd2c86c692a
Jacob <encody@users.noreply.github.com>
2021-07-06 17:53:37 -0500

    adds top senders/receivers charts

---
+++ src/services/charts/useTopIncomingChart.ts
@@ -0,0 +1,74 @@
+import { UnifiedTransactionAction } from '@/services/near/types';
+import {
+  BarSeriesOption,
+  ComposeOption,
+  TooltipComponentOption,
+} from 'echarts';
+import { ref, Ref, watch } from 'vue';
+
+type Option = ComposeOption<BarSeriesOption | TooltipComponentOption>;
+
+export function useTopIncomingChart(
+  actions: Ref<UnifiedTransactionAction[]>,
+): Ref<Option> {
+  const makeData = () => {
+    const groups = actions.value.reduce((acc, current) => {
+      const account = current.signer_account_id;
+      if (!acc[account]) {
+        acc[account] = 0;
+      }
+      acc[account]++;
+      return acc;
+    }, {} as Record<string, number>);
+
+    const ordered = Object.keys(groups).map(key => ({
+      name: key,
+      value: groups[key],
+    }));
+
+    ordered.sort((a, b) => b.value - a.value);
+
+    const slice = ordered.slice(0, 10).reverse();
+
+    return {
+      category: slice.map(x => x.name),
+      data: slice.map(x => x.value),
+    };
+  };
+
+  const genOption: () => Option = () => {
+    const g = makeData();
+    return {
+      grid: {
+        left: '13%',
+        containLabel: true,
+      },
+      tooltip: {
+        trigger: 'item',
+      },
+      xAxis: {
+        type: 'value',
+        boundaryGap: [0, 0.01],
+      },
+      yAxis: {
+        type: 'category',
+        data: g.category,
+      },
+      series: [
+        {
+          name: 'Accounts',
+          type: 'bar',
+          data: g.data,
+        },
+      ],
+    };
+  };
+
+  const option = ref(genOption());
+
+  watch(actions, () => {
+    option.value = genOption();
+  });
+
+  return option;
+}
--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -1,26 +1,33 @@
 <template>
+  <h1 class="text-2xl font-medium text-center">Action Types</h1>
   <VChart class="chart" theme="light" :option="actionTypeOption" />
+  <h1 class="text-2xl font-medium text-center">Top 10 Senders</h1>
+  <VChart class="chart" theme="light" :option="topIncomingOption" />
+  <h1 class="text-2xl font-medium text-center">Top 10 Receivers</h1>
+  <VChart class="chart" theme="light" :option="topOutgoingOption" />
 </template>
 
 <style scoped>
 .chart {
   height: 450px;
-  width: 450px;
+  width: 100%;
 }
 </style>
 
 <script lang="ts">
 import { useActionTypeChart } from '@/services/charts/useActionTypeChart';
+import { useTopIncomingChart } from '@/services/charts/useTopIncomingChart';
+import { useTopOutgoingChart } from '@/services/charts/useTopOutgoingChart';
 import { useRecentActions } from '@/services/near/useRecentActions';
 import { useNear } from '@/services/useNear';
 import { defineComponent } from '@vue/runtime-core';
 import { BarChart, PieChart } from 'echarts/charts';
-import { TooltipComponent } from 'echarts/components';
+import { GridComponent, TooltipComponent } from 'echarts/components';
 import { use } from 'echarts/core';
 import { CanvasRenderer } from 'echarts/renderers';
 import VChart from 'vue-echarts';
 
-use([CanvasRenderer, PieChart, BarChart, TooltipComponent]);
+use([CanvasRenderer, PieChart, BarChart, TooltipComponent, GridComponent]);
 
 export default defineComponent({
   components: {
@@ -30,9 +37,13 @@ export default defineComponent({
     const { account, network, timeframe } = useNear();
     const { actions } = useRecentActions({ account, network, timeframe });
     const actionTypeOption = useActionTypeChart(actions);
+    const topIncomingOption = useTopIncomingChart(actions);
+    const topOutgoingOption = useTopOutgoingChart(actions);
 
     return {
       actionTypeOption,
+      topIncomingOption,
+      topOutgoingOption,
     };
   },
 });
