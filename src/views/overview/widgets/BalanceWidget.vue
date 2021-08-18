c55164ec06e4cf997f48685f8f7f35ad25def0b3
Jacob <encody@users.noreply.github.com>
2021-08-18 11:05:25 -0500

    balance widget expand modal

--- src/views/overview/widgets/BalanceWidget.vue
+++ src/views/overview/widgets/BalanceWidget.vue
@@ -28,6 +28,7 @@
         listTitle="Recent transfers"
         buttonText="View transfers"
         :empty="transfers.length === 0"
+        @expand="expandModalOpen = true"
       >
         <template #header>
           <div class="text-2xl font-bold flex py-1">
@@ -37,7 +38,7 @@
           </div>
         </template>
         <template #default>
-          <template v-for="(transfer, i) in transfers" :key="i">
+          <template v-for="(transfer, i) in transfers.slice(0, 4)" :key="i">
             <div class="truncate">
               <account-link :account="transfer.account" />
             </div>
@@ -58,48 +59,134 @@
           </template>
         </template>
       </HeaderListButtonTemplate>
+      <Modal
+        :open="expandModalOpen"
+        title="Transfers"
+        @close="expandModalOpen = false"
+      >
+        <table>
+          <thead>
+            <tr>
+              <th>From</th>
+              <th>To</th>
+              <th>Amount</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(transfer, i) in transfers" :key="i">
+              <td class="truncate" style="max-width: 200px">
+                {{ transfer.action.signer_account_id }}
+              </td>
+              <td class="truncate" style="max-width: 200px">
+                {{ transfer.action.receiver_account_id }}
+              </td>
+              <td v-if="transfer.incoming" class="text-green-500 font-bold">
+                +{{
+                  $filters.number.standard(
+                    +$filters.toNear(transfer.amount).toString(),
+                  )
+                }}
+              </td>
+              <td v-else class="text-red-500 font-bold">
+                &minus;{{
+                  $filters.number.standard(
+                    +$filters.toNear(transfer.amount).toString(),
+                  )
+                }}
+              </td>
+              <td class="italic">
+                <a
+                  :href="
+                    networks[network].explorer +
+                    '/transactions/' +
+                    transfer.action.transaction_hash
+                  "
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <time
+                    :datetime="
+                      $filters.nearTimestampToISO(
+                        transfer.action.block_timestamp,
+                      )
+                    "
+                  >
+                    {{
+                      $filters.nearTimestampToLocaleString(
+                        transfer.action.block_timestamp,
+                        DateTime.DATETIME_MED,
+                      )
+                    }}
+                  </time>
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Modal>
     </template>
   </DashboardCard>
 </template>
 
 <script lang="ts">
+import Modal from '@/components/Modal.vue';
 import { useAccountView } from '@/composables/useAccountView';
 import { useNear } from '@/composables/useNear';
 import { useRecentActions } from '@/composables/useRecentActions';
-import { ActionKind, ITransferArgs } from '@/services/near/indexer/types';
+import { networks } from '@/services/near/indexer/networks';
+import {
+  ActionKind,
+  ITransferArgs,
+  UnifiedTransactionAction,
+} from '@/services/near/indexer/types';
+import { DateTime } from 'luxon';
 import { defineComponent, ref, watch } from 'vue';
 import DashboardCard from '../DashboardCard.vue';
 import HeaderListButtonTemplate from './HeaderListButtonTemplate.vue';
 
 export default defineComponent({
-  components: { DashboardCard, HeaderListButtonTemplate },
+  components: { DashboardCard, HeaderListButtonTemplate, Modal },
   setup() {
     const { account, network, timeframe } = useNear();
     const { actions } = useRecentActions({ account, network, timeframe });
     const { view } = useAccountView({ account, network, finality: 'final' });
     const transfers = ref(
-      [] as { account: string; amount: string; incoming: boolean }[],
+      [] as {
+        account: string;
+        amount: string;
+        incoming: boolean;
+        action: UnifiedTransactionAction;
+      }[],
     );
 
     watch([actions, account], ([actions, account]) => {
       const transferActions = actions.filter(
         action => action.action_kind === ActionKind.TRANSFER,
       );
-      transfers.value = transferActions
-        .map(action => {
-          const incoming = action.receiver_account_id === account;
-          return {
-            incoming,
-            account: incoming
-              ? action.signer_account_id
-              : action.receiver_account_id,
-            amount: (action.args as ITransferArgs).deposit,
-          };
-        })
-        .slice(0, 4);
+      transfers.value = transferActions.map(action => {
+        const incoming = action.receiver_account_id === account;
+        return {
+          incoming,
+          account: incoming
+            ? action.signer_account_id
+            : action.receiver_account_id,
+          amount: (action.args as ITransferArgs).deposit,
+          action,
+        };
+      });
     });
 
-    return { transfers, view };
+    const expandModalOpen = ref(false);
+
+    return {
+      transfers,
+      view,
+      expandModalOpen,
+      network,
+      networks,
+      DateTime,
+    };
   },
 });
 </script>
