25700d3ddfff764c0b8d214afae141df04045ed3
Jacob <encody@users.noreply.github.com>
2021-08-23 11:56:34 -0500

    story button dark theme

--- src/components/navigation/TopBar.vue
+++ src/components/navigation/TopBar.vue
@@ -61,8 +61,8 @@
         <span
           :class="[
             isActive
-              ? 'text-gray-700 bg-gray-400'
-              : 'text-gray-500 hover:text-gray-700',
+              ? 'text-gray-700 bg-gray-400 dark:text-gray-50'
+              : 'text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-50',
           ]"
           class="font-medium rounded px-3 py-1 bg-opacity-20"
           >story</span
