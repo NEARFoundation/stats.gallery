8ddf74f45e460bb9010da2ac1c6111c29f229f5a
Jacob <encody@users.noreply.github.com>
2021-08-23 16:13:57 -0500

    adds fancy terminal recruitment message

--- src/main.ts
+++ src/main.ts
@@ -53,3 +53,42 @@ app.config.globalProperties = {
     timeframeToPastTimestamp,
   },
 };
+
+const nearAscii = `
+     .,,,.                     .,,.     
+   .dXWWN0c.                 .oKWWXd.   
+   cNMMMMMWO;               ,OWMMMMNc   
+   lWMMMMMMMNd.           .cKMWWMMMWl   
+   lWMMWkdXMMWKl.        .dNNOlxWMMWl   
+   lWMMNc ,kNMMWO;      ;OXx;. cNMMWl   
+   lWMMNc  .c0WMMNx'   ,xo'    cNMMWl   
+   lWMMNc    .oXMMMKkl...      cNMMWl           NEAR Protocol
+   lWMMNc      ,kNMMMWO;       cNMMWl   
+   lWMMNc       .:0WMMMNx'     cNMMWl   Infrastructure for Innovation
+   lWMMNc     .:' .o0XMMWKl.   cNMMWl   
+   lWMMNc   ,d0d.   .,kNMMWO;  cNMMWl   
+   lWMMNl.:kNKc.      .:0WMMNx,lNMMWl   
+   lWMMWK0WWO,          .oXMMWXXWMMWl   
+   lWMMMMMNd.             ,kNMMMMMMWl   
+   ;KMMMMKc.               .:0WMMMMK;   
+    ,oxxl'                   .cdxxo,    
+                                        `;
+
+console.log(nearAscii);
+
+const bannerStyle =
+  'font-size: 16px;' +
+  'font-family: "DM Sans", Source Sans Pro, Helvetica, Helvetica Neue, sans-serif;' +
+  'font-weight: bold;' +
+  'background: #3399dd;' +
+  'background: linear-gradient(to right, #3399dd, #f18);' +
+  'color: white;' +
+  'text-shadow: 1px 1px 1px black;' +
+  'text-align: center;' +
+  'padding: 10px 15px;' +
+  'width: 100%;';
+
+console.log('%cNEAR is hiring!', bannerStyle);
+
+console.log('▶▶ Apply now ▶▶');
+console.log('https://near.org/careers/');
