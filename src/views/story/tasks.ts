1c0a41e74ea21ee00e11cd7140649e46920837e0
Warlord <brightness0719@gmail.com>
2021-09-21 20:21:41 +0300

    Near login

--- src/views/story/tasks.ts
+++ src/views/story/tasks.ts
@@ -10,7 +10,7 @@ export interface RoadmapTask {
 export const tasks: RoadmapTask[] = [
   {
     name: 'Community governance',
-    date: DateTime.fromObject({ year: 2021, month: 12, day: 20 }).toJSDate(),
+    date: DateTime.fromObject({ year: 2022, month: 1, day: 31 }).toJSDate(),
     done: false,
     link: '',
   },
@@ -33,21 +33,27 @@ export const tasks: RoadmapTask[] = [
     link: 'https://github.com/NEAR-Edu/stats.gallery/issues/39',
   },
   {
-    name: 'Make everything sharable!',
-    date: DateTime.fromObject({ year: 2021, month: 9, day: 13 }).toJSDate(),
-    done: true,
+    name: 'NEAR Login',
+    date: DateTime.fromObject({ year: 2021, month: 9, day: 30 }).toJSDate(),
+    done: false,
+    link: 'https://github.com/NEAR-Edu/stats.gallery/issues/47',
+  },
+  {
+    name: 'New charts',
+    date: DateTime.fromObject({ year: 2021, month: 9, day: 29 }).toJSDate(),
+    done: false,
     link: '',
   },
   {
     name: 'Leaderboards page',
     date: DateTime.fromObject({ year: 2021, month: 9, day: 20 }).toJSDate(),
-    done: false,
+    done: true,
     link: '',
   },
   {
-    name: 'New charts',
-    date: DateTime.fromObject({ year: 2021, month: 9, day: 20 }).toJSDate(),
-    done: false,
+    name: 'Make everything sharable!',
+    date: DateTime.fromObject({ year: 2021, month: 9, day: 13 }).toJSDate(),
+    done: true,
     link: '',
   },
   {
