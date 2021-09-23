121440e323d7344502194938cf9d100b41b8d936
Jacob <encody@users.noreply.github.com>
2021-09-23 15:31:51 -0500

    teleport tooltips

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <router-view></router-view>
+  <div id="tooltips"></div>
 </template>
 
 <style>
--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,49 +1,53 @@
 <template>
   <client-only>
     <Popover v-slot="{ open }" class="sm:relative">
-      <PopoverButton>
+      <PopoverButton ref="buttonRef">
         <slot name="trigger" :open="open" />
       </PopoverButton>
-      <transition
-        enter-active-class="transition duration-200 ease-out"
-        enter-from-class="translate-y-1 opacity-0"
-        enter-to-class="translate-y-0 opacity-100"
-        leave-active-class="transition duration-150 ease-in"
-        leave-from-class="translate-y-0 opacity-100"
-        leave-to-class="translate-y-1 opacity-0"
-      >
-        <PopoverPanel
-          ref="panelRef"
-          class="
-            absolute
-            sm:top-full
-            left-0
-            sm:left-1/2 sm:transform sm:-translate-x-1/2
-            z-50
-            w-full
-            sm:w-screen
-            mt-1
-            sm:max-w-sm
-          "
+      <teleport to="#tooltips">
+        <transition
+          enter-active-class="transition duration-200 ease-out"
+          enter-from-class="translate-y-1 opacity-0"
+          enter-to-class="translate-y-0 opacity-100"
+          leave-active-class="transition duration-150 ease-in"
+          leave-from-class="translate-y-0 opacity-100"
+          leave-to-class="translate-y-1 opacity-0"
         >
-          <div
-            ref="elRef"
+          <PopoverPanel
+            ref="panelRef"
             class="
-              overflow-hidden
-              rounded-md
-              shadow-lg
-              bg-gray-900 bg-opacity-80
-              backdrop-filter backdrop-blur-sm
-              text-white
-              p-3
-              mx-4
-              sm:mx-0
+              fixed
+              z-50
+              w-full
+              sm:w-screen sm:max-w-sm
+              mt-1
+              transform
+              -translate-x-1/2
             "
+            :style="{
+              left: left + 'px',
+              top: top + 'px',
+            }"
           >
-            <slot name="content" :open="open" />
-          </div>
-        </PopoverPanel>
-      </transition>
+            <div
+              ref="elRef"
+              class="
+                overflow-hidden
+                rounded-md
+                shadow-lg
+                bg-gray-900 bg-opacity-80
+                backdrop-filter backdrop-blur-sm
+                text-white
+                p-3
+                mx-4
+                sm:mx-0
+              "
+            >
+              <slot name="content" :open="open" />
+            </div>
+          </PopoverPanel>
+        </transition>
+      </teleport>
     </Popover>
   </client-only>
 </template>
@@ -61,6 +65,10 @@ export default defineComponent({
   setup() {
     const panelRef = ref<VNode | null>(null);
     const elRef = ref<HTMLElement | null>(null);
+    const buttonRef = ref<VNode | null>(null);
+
+    const left = ref(0);
+    const top = ref(0);
 
     onMounted(() => {
       const fit = () => {
@@ -73,9 +81,20 @@ export default defineComponent({
         }
       };
 
+      const reposition = () => {
+        if (buttonRef.value && buttonRef.value.el) {
+          const el = buttonRef.value.el as HTMLButtonElement;
+          const rect = el.getBoundingClientRect();
+          top.value = rect.top + rect.height;
+          left.value = rect.left;
+        }
+      };
+
       fit();
 
       window.addEventListener('resize', fit);
+      window.addEventListener('resize', reposition);
+      window.addEventListener('scroll', reposition);
 
       watch(elRef, fit);
     });
@@ -83,6 +102,9 @@ export default defineComponent({
     return {
       panelRef,
       elRef,
+      buttonRef,
+      left,
+      top,
     };
   },
 });
